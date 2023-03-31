1d7b86e6bf0169370176e15f7a3e32c930e2aa01
mymusise <mymusise1@gmail.com>
2023-03-31 22:27:05 +0800

    It looks like the training issue with the official code has been fixed, use it as deafault.
    Signed-off-by: mymusise <mymusise1@gmail.com>

--- finetune.py
+++ finetune.py
@@ -2,8 +2,7 @@ from transformers.integrations import TensorBoardCallback
 from torch.utils.tensorboard import SummaryWriter
 from transformers import TrainingArguments
 from transformers import Trainer, HfArgumentParser
-from transformers import AutoTokenizer
-from modeling_chatglm import ChatGLMForConditionalGeneration
+from transformers import AutoTokenizer, AutoModel
 import torch
 import torch.nn as nn
 from peft import get_peft_model, LoraConfig, TaskType
@@ -27,72 +26,26 @@ class CastOutputToFloat(nn.Sequential):
         return super().forward(x).to(torch.float32)
 
 
-def get_masks_and_position_ids(
-    seq, seq_len, context_length, device, gmask=False, position_encoding_2d=True
-):
-    mask_position = (
-        seq_len - 2
-    )  # is equal to `seq.index(mask_token)` or `seq.index(150001)`
-    attention_mask = torch.ones((1, context_length, context_length), device=device)
-    attention_mask.tril_()
-    attention_mask[..., : mask_position - 1] = 1
-    attention_mask = (attention_mask < 0.5).bool()
-
-    if position_encoding_2d:
-        seq_length = seq_len - 1  # is equal to `seq_length = seq.index(150004)`
-        position_ids = torch.arange(context_length, dtype=torch.long, device=device)
-        if not gmask:
-            position_ids[seq_length:] = mask_position
-        block_position_ids = torch.cat(
-            (
-                torch.zeros(seq_length, dtype=torch.long, device=device),
-                torch.arange(
-                    context_length - seq_length, dtype=torch.long, device=device
-                )
-                + 1,
-            )
-        )
-        position_ids = torch.stack((position_ids, block_position_ids), dim=0)
-    else:
-        position_ids = torch.arange(context_length, dtype=torch.long, device=device)
-        if not gmask:
-            position_ids[context_length - 1 :] = mask_position
-    return attention_mask, position_ids
-
-
 def data_collator(features: list) -> dict:
     len_ids = [len(feature["input_ids"]) for feature in features]
     longest = max(len_ids)
     input_ids = []
-    attention_mask_list = []
-    position_ids_list = []
     labels_list = []
     for ids_l, feature in sorted(zip(len_ids, features), key=lambda x: -x[0]):
         ids = feature["input_ids"]
         seq_len = feature["seq_len"]
         labels = (
-            [-100] * (seq_len - 1)
-            + ids[(seq_len - 1) :]
-            + [-100] * (longest - ids_l)
+            [-100] * (seq_len - 1) + ids[(seq_len - 1) :] + [-100] * (longest - ids_l)
         )
         ids = ids + [tokenizer.pad_token_id] * (longest - ids_l)
         _ids = torch.LongTensor(ids)
-        attention_mask, position_ids = get_masks_and_position_ids(
-            ids, seq_len, longest, _ids.device, gmask=False
-        )
         labels_list.append(torch.LongTensor(labels))
         input_ids.append(_ids)
-        attention_mask_list.append(attention_mask)
-        position_ids_list.append(position_ids)
     input_ids = torch.stack(input_ids)
     labels = torch.stack(labels_list)
-    attention_mask = torch.stack(attention_mask_list)
-    position_ids = torch.stack(position_ids_list)
     return {
         "input_ids": input_ids,
         "labels": labels,
-        "attention_mask": attention_mask,
-        "position_ids": position_ids,
     }
 
 
@@ -100,13 +53,12 @@ class ModifiedTrainer(Trainer):
     def compute_loss(self, model, inputs, return_outputs=False):
         return model(
             input_ids=inputs["input_ids"],
-            attention_mask=inputs["attention_mask"],
-            position_ids=inputs["position_ids"],
             labels=inputs["labels"],
         ).loss
 
     def save_model(self, output_dir=None, _internal_call=False):
         from transformers.trainer import TRAINING_ARGS_NAME
+
         os.makedirs(output_dir, exist_ok=True)
         torch.save(self.args, os.path.join(output_dir, TRAINING_ARGS_NAME))
         saved_params = {
@@ -122,7 +74,7 @@ def main():
     ).parse_args_into_dataclasses()
 
     # init model
-    model = ChatGLMForConditionalGeneration.from_pretrained(
+    model = AutoModel.from_pretrained(
         "THUDM/chatglm-6b", load_in_8bit=True, trust_remote_code=True, device_map="auto"
     )
     model.gradient_checkpointing_enable()
@@ -146,6 +98,7 @@ def main():
 
     # load dataset
     dataset = datasets.load_from_disk(finetune_args.dataset_path)
+    print(f"\n{len(dataset)=}\n")
 
     # start train
     trainer = ModifiedTrainer(

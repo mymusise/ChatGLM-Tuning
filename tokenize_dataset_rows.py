0f513c1ab177e7e2b203b50d1d99a2e6ca86904e
duyupeng <656341393@example.com>
2023-11-05 10:11:29 +0800

    提交了对chatglm2的预处理

--- tokenize_dataset_rows.py
+++ tokenize_dataset_rows.py
@@ -6,19 +6,6 @@ import datasets
 import transformers
 
 
-# def preprocess(tokenizer, config, example, max_seq_length):
-#     prompt = example["context"]
-#     target = example["target"]
-#     prompt_ids = tokenizer.encode(prompt, max_length=max_seq_length, truncation=True)
-#     target_ids = tokenizer.encode(
-#         target,
-#         max_length=max_seq_length,
-#         truncation=True,
-#         add_special_tokens=False)
-#     input_ids = prompt_ids + target_ids + [config.eos_token_id]
-#     return {"input_ids": input_ids, "seq_len": len(prompt_ids)}
-
-
 def preprocess(tokenizer, config, example, max_seq_length):
     prompt = example["context"]
     target = example["target"]
@@ -30,6 +17,9 @@ def preprocess(tokenizer, config, example, max_seq_length):
         add_special_tokens=False)
     input_ids = prompt_ids + target_ids + [config.eos_token_id]
     return {"input_ids": input_ids, "seq_len": len(prompt_ids)}
+
+
+
 
 
 def preprocess_chatglm2(tokenizer, config, example, max_seq_length):
